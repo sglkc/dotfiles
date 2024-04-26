1a0bef2df9d5fdc612802d6a8f10d70ebec1d7fa
Seya <31957516+sglkc@users.noreply.github.com>
2024-04-26 23:40:21 +0700

    nvim(plugins): add tabout
    nvim(ultimate-autopairs): auto comma for brackets
    nvim(nvim-cmp): disable super tab
    nvim(comment): change key without motion

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -3,5 +3,37 @@ return {
     'altermo/ultimate-autopair.nvim',
     event = { 'InsertEnter', 'CmdlineEnter' },
     branch = 'v0.6', --recommended as each new version will have breaking changes
+    config = function()
+      -- https://github.com/altermo/ultimate-autopair.nvim/issues/46
+      local default=require'ultimate-autopair.profile.default.utils'
+      local utils=require'ultimate-autopair.utils'
+      local function create_map()
+        local m={}
+        m.doc='ultimate-autopair , map'
+        m.p=10
+        m.check=function (o)
+          local begin = o.line:sub(o.col-1,o.col-1)
+          if o.mode=='i' and o.key==',' and (begin=='{' or begin=='(' or begin=='[')then
+            local pair,col,row=default.get_pair_and_end_pair_pos_from_start(o,o.col,true)
+            if not pair then return end
+            return utils.create_act{
+              {'j',row-o.row },{'home'},
+              {'l',col},
+              ',',
+              {'k',row-o.row},{'home'},
+              {'l',o.col-1},
+            }
+          end
+        end
+        m.get_map=function (mode) if mode=='i' then return {','} end end
+        return m
+      end
+      require'ultimate-autopair'.init({
+        require'ultimate-autopair'.extend_default({}),
+        {
+          profile='raw',
+          create_map(),
+        }})
+    end
   }
 }
