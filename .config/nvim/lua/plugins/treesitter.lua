0baf9ba107006402da7a5d0ebd81f04bec642477
Seya <31957516+sglkc@users.noreply.github.com>
2024-07-09 11:46:46 +0700

    nvim(treesitter): add blade grammar

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -71,6 +71,16 @@ return {
     },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
+      require("nvim-treesitter.parsers").get_parser_configs().blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+
+      vim.filetype.add({ pattern = { [".*%.blade%.php"] = "blade" } })
     end
   }
 }
