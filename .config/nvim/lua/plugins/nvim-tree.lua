0c9807a3135d54dc4101f13e3799ea22b119b772
Seya <31957516+sglkc@users.noreply.github.com>
2025-06-05 15:14:11 +0700

    nvim(plugins): add project.nvim, disable autochdir

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -112,6 +112,12 @@ return {
       modified = {
         enable = true
       },
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true
+      },
     }
   }
 }
