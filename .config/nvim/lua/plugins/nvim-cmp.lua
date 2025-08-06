163db326817b6eeb6075b48cab7bf6d2686961f5
Seya <31957516+sglkc@users.noreply.github.com>
2025-08-06 07:22:26 +0700

    nvim(plugins): remove zeioth/garbage-day.nvim
    nvim(cmp): fix enter behavior
    nvim(lsp): set unfocusable

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -123,17 +123,15 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping({
-            i = function(fallback)
-              if cmp.visible() and cmp.get_active_entry() then
-                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-              else
-                fallback()
-              end
-            end,
-            s = cmp.mapping.confirm({ select = true }),
-            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-          }),
+          ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm({
+                select = true,
+              })
+            else
+              fallback()
+            end
+          end),
         },
       }
     end,
