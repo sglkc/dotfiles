80f7de9ca5539f6974ab7f32f0253b7abe552d09
Seya <31957516+sglkc@users.noreply.github.com>
2025-12-30 16:44:43 +0700

    nvim(treesitter): update config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,86 +1,67 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    version = false, -- main branch (rewrite)
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+
     dependencies = {
-      'windwp/nvim-ts-autotag',
-      'RRethy/nvim-treesitter-endwise',
-      'jwalton512/vim-blade',
-      -- 'hiphish/rainbow-delimiters.nvim',
+      "windwp/nvim-ts-autotag",
+      "RRethy/nvim-treesitter-endwise",
+      "jwalton512/vim-blade",
       {
-        'andymass/vim-matchup',
+        "andymass/vim-matchup",
         lazy = false,
         config = function()
-          vim.g.matchup_matchparen_offscreen = { method = 'popup' }
-          vim.g.matchup_transmute_enabled = 1 -- html element replace
-
+          vim.g.matchup_matchparen_offscreen = { method = "popup" }
+          vim.g.matchup_transmute_enabled = 1
           vim.g.matchup_delim_noskips = 1
-          vim.g.matchup_delim_stopline = 1000          -- 1500
-          vim.g.matchup_matchparen_stopline = 200      -- 400
-
-          vim.g.matchup_matchparen_timeout = 100       -- 300
-          vim.g.matchup_matchparen_insert_timeout = 60 -- 60
-
+          vim.g.matchup_delim_stopline = 1000
+          vim.g.matchup_matchparen_stopline = 200
+          vim.g.matchup_matchparen_timeout = 100
+          vim.g.matchup_matchparen_insert_timeout = 60
           vim.g.matchup_matchparen_deferred = 1
-          vim.g.matchup_matchparen_deferred_show_delay = 50  -- 50
-          vim.g.matchup_matchparen_deferred_hide_delay = 300 -- 700
+          vim.g.matchup_matchparen_deferred_show_delay = 50
+          vim.g.matchup_matchparen_deferred_hide_delay = 300
           vim.g.matchup_matchparen_pumvisible = 0
 
           local ok, cmp = pcall(require, "cmp")
           if ok then
-            cmp.event:on("menu_opened", function() vim.b.matchup_matchparen_enabled = false end)
-            cmp.event:on("menu_closed", function() vim.b.matchup_matchparen_enabled = true end)
-          end
-        end
-      }
-    },
-    lazy = true,
-    version = false,
-    build = ":TSUpdate",
-    event = "VeryLazy",
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall", "TSUninstall" },
-    opts = {
-      ensure_installed = {
-        --[[ "comment", ]] "todotxt",
-        "bash",
-        "python",
-        "html", "javascript", "jsdoc", "css", "typescript",
-        "tsx", "astro", "vue",
-        "php", "php_only", "phpdoc",
-        "sql",
-        "json", "jsonc",
-        "markdown", "markdown_inline",
-        "query",
-        "regex",
-        "lua",
-        "vim", "vimdoc",
-        "toml", "yaml",
-      },
-      sync_install = true,
-      highlight = {
-        enable = true,
-        disable = function(lang, buf)
-          local max_filesize = 1000 * 1024 -- 3000 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
+            cmp.event:on("menu_opened", function()
+              vim.b.matchup_matchparen_enabled = false
+            end)
+            cmp.event:on("menu_closed", function()
+              vim.b.matchup_matchparen_enabled = true
+            end)
           end
         end,
-        -- additional_vim_regex_highlighting = true,
       },
-      indent = {
-        enable = true
-      },
-      matchup = {
-        enable = true
-      },
-      endwise = {
-        enable = true
-      }
     },
-    config = function(_, opts)
-      local parsers = require("nvim-treesitter.parsers")
-      require("nvim-ts-autotag").setup()
 
+    opts = {
+      ensure_installed = {
+        "bash",
+        "python",
+        "html", "css",
+        "javascript", "jsdoc",
+        "typescript", "tsx",
+        "astro", "vue",
+        "php", "phpdoc",
+        "sql",
+        "json", "jsonc", "markdown", "markdown_inline",
+        "query", "regex",
+        "lua", "vim", "vimdoc", "toml", "yaml",
+      },
+    },
+
+    config = function(_, opts)
+      local ts = require("nvim-treesitter")
+
+      -- 1. Install parsers (rewrite API)
+      ts.install(opts.ensure_installed)
+
+      -- 2. Blade parser (unchanged, still valid)
+      local parsers = require("nvim-treesitter.parsers")
       parsers.blade = {
         install_info = {
           url = "https://github.com/EmranMR/tree-sitter-blade",
@@ -90,7 +71,35 @@ return {
         filetype = "blade",
       }
 
-      vim.filetype.add({ pattern = { [".*%.blade%.php"] = "blade" } })
-    end
-  }
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+      -- 3. Feature plugins
+      require("nvim-ts-autotag").setup()
+
+      -- 4. Treesitter activation (NEW REQUIRED STEP)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "*",
+        callback = function(args)
+          local buf = args.buf
+          local max_filesize = 1000 * 1024 -- ~1MB
+
+          local ok, stat = pcall(
+            vim.loop.fs_stat,
+            vim.api.nvim_buf_get_name(buf)
+          )
+
+          if ok and stat and stat.size > max_filesize then
+            return
+          end
+
+          pcall(vim.treesitter.start, buf)
+        end,
+      })
+    end,
+  },
 }
+
